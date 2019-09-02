56ddad4a1c08d182dd6f0afbdb417cfa246110b8
parvejleelgar <parvejchauhan23@gmail.com>
2019-09-02 15:27:54 +0530

    Product Report

--- src/main/java/com/axelor/apps/gst/report/IReport.java
+++ src/main/java/com/axelor/apps/gst/report/IReport.java
@@ -3,4 +3,5 @@ package com.axelor.apps.gst.report;
 public interface IReport {
 
 	public static final String GST_INVOICE = "GstInvoice.rptdesign";
+	public static final String GST_PRODUCT = "GstProduct.rptdesign";
 }
